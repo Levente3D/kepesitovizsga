38226975ddbee24fecbfc6bb374065bb2cddc59a
Levente3D <faylevente@gmail.com>
2021-04-29 12:54:54 +0200

    Phonebook end_commit

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
@@ -0,0 +1,23 @@
+//package hu.nive.ujratervezes.kepesitovizsga.phonebook;
+//
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Phonebook {
+//
+//    public void exportPhonebook(Map<String, String> contacts, String outputPath) {
+//
+//        try {
+//            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+//                System.out.println(entry.getKey() + ": " + entry.getValue());
+//            }
+//
+//        } catch (IOException ioe) {
+//            throw new IllegalArgumentException("Phonebook is empty");
+//        }
+//
+//    }
+//
+//}
+//
